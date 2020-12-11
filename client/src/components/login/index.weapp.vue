3bce6081a972bcd5240d8a9f8246eb5fcf167776
lining02 <lining02@chehang168.com>
2020-12-11 14:22:00 +0800

    first commit

---
+++ client/src/components/login/index.weapp.vue
@@ -0,0 +1,35 @@
+<template>
+    <view className='index'>
+        <!-- <AtButton @tap="getLogin">获取登录云函数</AtButton> -->
+        <text>context：{{context}}</text>
+    </view>
+</template>
+
+<script>
+import Taro from '@tarojs/taro'
+// import { AtButton } from "taro-ui-vue3"
+
+export default {
+    name: 'login',
+    components: {
+        // AtButton
+    },
+    data(){
+        return {
+            context: {}
+        }
+    },
+    methods: {
+        getLogin () {
+            Taro.cloud
+                .callFunction({
+                    name: "login",
+                    data: {}
+                })
+                .then(res => {
+                    this.context = JSON.stringify(res.result)
+                })
+        }
+    },
+}
+</script>
